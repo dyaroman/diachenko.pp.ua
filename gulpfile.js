3ce78f5369a927ecd854c228c00114c88983df55
Roman Diachenko <dyaroman@gmail.com>
2019-02-20 10:21:47 +0200

    code refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const puppeteer = require('puppeteer');
 
 const htmlTask = require('./gulp-tasks/html');
 const stylesTask = require('./gulp-tasks/styles');
-const jsTask = require('./gulp-tasks/scripts');
 const imagesTask = require('./gulp-tasks/images');
 
 gulp.task('html', () => {
@@ -17,12 +16,6 @@ gulp.task('html', () => {
 gulp.task('css', () => {
   console.log(`build css`);
   return stylesTask.styles();
-});
-
-gulp.task('js', () => {
-  console.log(`build js`);
-  jsTask.jsLibs();
-  return jsTask.scripts();
 });
 
 gulp.task('images', () => {
@@ -40,7 +33,6 @@ gulp.task('server', () => {
 gulp.task('watch', () => {
   htmlTask.htmlWatcher();
   stylesTask.cssWatcher();
-  jsTask.jsWatcher();
   imagesTask.imagesWatcher();
   gulp.task('server')();
 });
@@ -75,7 +67,6 @@ gulp.task('copy', () => {
 gulp.task('build', (cb) => {
   gulp.task('html')();
   gulp.task('css')();
-  gulp.task('js')();
   gulp.task('images')();
   gulp.task('pdf')();
   gulp.task('copy')();
