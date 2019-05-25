f9d5b46e6e01717cb49cd57d1bf91fd97c791ef6
Roman Diachenko <dyaroman@gmail.com>
2019-05-25 08:36:37 +0300

    add description

--- assets/options.js
+++ assets/options.js
@@ -2,4 +2,6 @@ module.exports = {
   "fullName": "Roman Diachenko",
   "firstName": "Roman",
   "lastName": "Diachenko",
+  "description": "Roman Diachenko is a frontend developer.",
+  "username": "dyaroman"
 };
