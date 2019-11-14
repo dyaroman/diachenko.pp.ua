6d7544824f427e9900601f982fba23403306bae6
Roman Diachenko <romand@leadsmarket.com>
2019-11-14 11:53:35 +0200

    update '.editorconfig'

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -15,25 +15,25 @@ const config = require('./_config');
  * minification styles only for prod env
  */
 const styles = () => gulp.src([
-  `./src/css/**/*.less`,
-  `!./src/css/**/_*.less`
-  ])
-  .pipe(config.production ? util.noop() : sourcemaps.init({
-    loadMaps: true
-  }))
-  .pipe(less())
-  .on('error', notify.onError())
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions', 'ie 11'],
-    cascade: false
-  }))
-  .pipe(config.production ? util.noop() : sourcemaps.write())
-  .pipe(config.production ? csso() : util.noop())
-  .pipe(gulp.dest(`./dest/css`));
+    `./src/css/**/*.less`,
+    `!./src/css/**/_*.less`
+])
+    .pipe(config.production ? util.noop() : sourcemaps.init({
+        loadMaps: true
+    }))
+    .pipe(less())
+    .on('error', notify.onError())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie 11'],
+        cascade: false
+    }))
+    .pipe(config.production ? util.noop() : sourcemaps.write())
+    .pipe(config.production ? csso() : util.noop())
+    .pipe(gulp.dest(`./dest/css`));
 
 const watcher = () => {
-  console.log(`watch css in './src/css/**/*.less'`);
-  return gulp.watch(`./src/css/**/*.less`, styles);
+    console.log(`watch css in './src/css/**/*.less'`);
+    return gulp.watch(`./src/css/**/*.less`, styles);
 };
 
 const cssWatcher = gulp.series(styles, watcher);
